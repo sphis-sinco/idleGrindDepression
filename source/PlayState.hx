8fa490db49a50f7eaf3681675f8377aaf62eaa19
Viwu <sinconsistencia@gmail.com>
2025-12-07 10:29:41 -0600

    versionText

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,19 @@
 package;
 
+import lime.app.Application;
+import flixel.text.FlxText;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var versionText:FlxText = new FlxText(2, 2, 0, Application.current.meta.get("version"), 8);
+
 	override public function create()
 	{
 		super.create();
+
+		versionText.alpha = 0.25;
+		add(versionText);
 	}
 
 	override public function update(elapsed:Float)
