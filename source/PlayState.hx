c2ccdd61af2c96918c631e352d68ec96c3d73238
Viwu <sinconsistencia@gmail.com>
2025-12-07 10:44:00 -0600

    player exists

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxSprite;
 import lime.app.Application;
 import flixel.text.FlxText;
 import flixel.FlxState;
@@ -8,9 +9,23 @@ class PlayState extends FlxState
 {
 	public var versionText:FlxText = new FlxText(2, 2, 0, Application.current.meta.get("version"), 8);
 
+	public var player:FlxSprite;
+	public var playerState:String;
+
+	public function setPlayerState(state:String):Void
+	{
+		player.loadGraphic('assets/images/player/' + state + '.png');
+		playerState = state;
+	}
+
 	override public function create()
 	{
 		super.create();
+
+		player = new FlxSprite();
+		setPlayerState('idle');
+		player.screenCenter();
+		add(player);
 
 		versionText.alpha = 0.25;
 		add(versionText);
