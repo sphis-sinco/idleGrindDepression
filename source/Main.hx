a2efc3812e633e30a1c105f11b0b4a25e9495e22
Viwu <sinconsistencia@gmail.com>
2025-12-07 11:57:20 -0600

    version text above all and is everywhere and position the shop button

--- source/Main.hx
+++ source/Main.hx
@@ -5,9 +5,14 @@ import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	public static var versionText:FlxText = new FlxText(2, 2, 0, Application.current.meta.get("version"), 8);
+
 	public function new()
 	{
 		super();
 		addChild(new FlxGame(0, 0, PlayState));
+
+		versionText.alpha = 0.25;
+		addChild(versionText);
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -8,8 +8,6 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
-	public var versionText:FlxText = new FlxText(2, 2, 0, Application.current.meta.get("version"), 8);
-
 	public var player:PixelSprite;
 	public var playerState:PlayerStates;
 
@@ -38,9 +36,9 @@ class PlayState extends FlxState
 
 		shopButton = new FlxButton(0,0, "Shop", () -> FlxG.switchState(() -> new Shop()));
 		add(shopButton);
-
-		versionText.alpha = 0.25;
-		add(versionText);
+		
+		shopButton.screenCenter();
+		shopButton.y += shopButton.height;
 	}
 
 	override public function update(elapsed:Float)
