7d8304c3648b5df498bbd96df4c9b4b0c3dfe351
Viwu <sinconsistencia@gmail.com>
2025-12-07 11:54:59 -0600

    shop

---
+++ source/KeeperStates.hx
@@ -0,0 +1,5 @@
+enum abstract KeeperStates(String) {
+    var idle = 'idle';
+    var cant_afford = 'cant_afford';
+    var good_choice = 'good_choice';
+}
--- source/Shop.hx
+++ source/Shop.hx
@@ -3,16 +3,33 @@ import flixel.FlxState;
 
 class Shop extends FlxState
 {
+	public var desk:PixelSprite;
+	public var keeper:PixelSprite;
+
+	public function setKeeperState(state:KeeperStates)
+		keeper.loadGraphic('assets/images/shop/' + state + '.png');
+
 	override function create()
 	{
 		super.create();
+
+		keeper = new PixelSprite();
+		add(keeper);
+
+		setKeeperState(idle);
+		keeper.screenCenter();
+
+		desk = new PixelSprite();
+		add(desk);
+
+		desk.screenCenter();
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-        PlayState.tick++;
+		PlayState.tick++;
 		FlxG.watch.addQuick('Tick', PlayState.tick);
 	}
 }
